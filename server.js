04dff222e26626d8714e2225217d65ff0dced6b0
HishamAhmed146 <HishamAhmed146>
2023-08-21 08:59:57 +0500

    updated server.js

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.use((err, req, res, next) => {
 
   return res.status(errorStatus).send(errorMessage);
 });
-
-app.listen(8800, () => {
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
   connect();
   console.log("Backend server is running!");
 });
